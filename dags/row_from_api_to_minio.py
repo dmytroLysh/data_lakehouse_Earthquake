1b55ae2dfe75f676ed4e64f383bee579000ba9ba
Dmytro Lyshtva <lyshtva.dmytro+github@gmail.com>
2025-08-19 20:47:57 +0200

    update dag for preinstalation

--- dags/row_from_api_to_minio.py
+++ dags/row_from_api_to_minio.py
@@ -1,13 +1,14 @@
 import logging
-import pendulum
+
 import duckdb
+import pendulum
 from airflow import DAG
+from airflow.models import Variable
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
-from airflow.models import Variable
 
 OWNER = "d.lyshtva"
-DAG_ID = "row_from_api_to_minio"
+DAG_ID = "raw_from_api_to_minio"
 
 LAYER = "raw"
 SOURCE = "earthquake"
@@ -15,71 +16,85 @@ SOURCE = "earthquake"
 ACCESS_KEY = Variable.get("access_key")
 SECRET_KEY = Variable.get("secret_key")
 
+LONG_DESCRIPTION = """
+# LONG DESCRIPTION
+"""
+
+SHORT_DESCRIPTION = "SHORT DESCRIPTION"
+
 args = {
     "owner": OWNER,
+    "start_date": pendulum.datetime(2025, 8, 1, tz="Europe/Madrid"),
+    "catchup": True,
     "retries": 3,
     "retry_delay": pendulum.duration(hours=1),
 }
 
 
 def get_dates(**context) -> tuple[str, str]:
+    """"""
     start_date = context["data_interval_start"].format("YYYY-MM-DD")
     end_date = context["data_interval_end"].format("YYYY-MM-DD")
+
     return start_date, end_date
 
 
 def get_and_transfer_api_data_to_minio(**context):
+    """"""
+
     start_date, end_date = get_dates(**context)
     logging.info(f"💻 Start load for dates: {start_date}/{end_date}")
-
     con = duckdb.connect()
-    try:
-        sql = f"""
+
+    con.sql(
+        f"""
         SET TIMEZONE='UTC';
         INSTALL httpfs;
         LOAD httpfs;
-
         SET s3_url_style = 'path';
         SET s3_endpoint = 'minio:9000';
         SET s3_access_key_id = '{ACCESS_KEY}';
         SET s3_secret_access_key = '{SECRET_KEY}';
         SET s3_use_ssl = FALSE;
 
-        COPY (
-            SELECT *
-            FROM read_csv_auto(
-              'https://earthquake.usgs.gov/fdsnws/event/1/query?format=csv&starttime={start_date}&endtime={end_date}'
-            )
-        )
-        TO 's3://prod/{LAYER}/{SOURCE}/{{{{ ds }}}}/part-000.parquet'
-        WITH (FORMAT PARQUET, COMPRESSION ZSTD);
-        """
-        con.sql(sql)
-    finally:
-        con.close()
+        COPY
+        (
+            SELECT
+                *
+            FROM
+                read_csv_auto('https://earthquake.usgs.gov/fdsnws/event/1/query?format=csv&starttime={start_date}&endtime={end_date}') AS res
+        ) TO 's3://prod/{LAYER}/{SOURCE}/{start_date}/{start_date}_00-00-00.gz.parquet';
 
-    logging.info(f"✅ Ingest finished for {start_date}")
+        """,
+    )
+
+    con.close()
+    logging.info(f"✅ Download for date success: {start_date}")
 
 
 with DAG(
     dag_id=DAG_ID,
-    description="Ingest raw earthquake data from public API to S3/MinIO as Parquet.",
-    start_date=pendulum.datetime(2025, 5, 1, tz="UTC"),
-    schedule="0 5 * * *",
-    catchup=True,
+    schedule_interval="0 5 * * *",
     default_args=args,
     tags=["s3", "raw"],
+    description=SHORT_DESCRIPTION,
+    concurrency=1,
     max_active_tasks=1,
     max_active_runs=1,
-    concurrency=1,
 ) as dag:
-    start = EmptyOperator(task_id="start")
+    dag.doc_md = LONG_DESCRIPTION
 
-    ingest_task = PythonOperator(
+    start = EmptyOperator(
+        task_id="start",
+    )
+
+    get_and_transfer_api_data_to_s3 = PythonOperator(
         task_id="get_and_transfer_api_data_to_minio",
         python_callable=get_and_transfer_api_data_to_minio,
     )
 
-    end = EmptyOperator(task_id="end")
+    end = EmptyOperator(
+        task_id="end",
+    )
 
-    start >> ingest_task >> end
+    start >> get_and_transfer_api_data_to_s3 >> end
